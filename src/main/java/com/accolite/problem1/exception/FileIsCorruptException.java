2a2bb36e2ed55d6a85a0543b7584c364cb6dd27a
paras <paras.agarwal@accolitedigital.com>
2022-11-04 13:09:06 +0530

    Initial Commit3

---
+++ src/main/java/com/accolite/problem1/exception/FileIsCorruptException.java
@@ -0,0 +1,4 @@
+package com.accolite.problem1.exception;
+
+public class FileIsCorruptException extends RuntimeException{
+}
---
+++ src/main/java/com/accolite/problem1/exception/FileTypeNotSupportedException.java
@@ -0,0 +1,4 @@
+package com.accolite.problem1.exception;
+
+public class FileTypeNotSupportedException extends RuntimeException{
+}
